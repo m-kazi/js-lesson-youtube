15cd10656c9ae8b4eccf950208c0bcd89f9193ae
Kazz <38118958+m-kazi@users.noreply.github.com>
2023-12-15 12:17:34 +0000

    conversion of data types

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueIndex = Number(score)
+//console.log(typeof valueIndex);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1 ; false => 0
+
+let loggedIn = 0
+//console.log(loggedIn);
+
+
+let booleanIsLoggedIn = Boolean(loggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "tanim" => true
+
+
+let stringNumber = 33
+console.log(typeof stringNumber);
+
+let convertedString = String(stringNumber)
+console.log(typeof convertedString);
+
