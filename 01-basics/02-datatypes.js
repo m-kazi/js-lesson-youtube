aaf91273ff3f799be0d8462438ce42ec3323bbfa
Kazz <38118958+m-kazi@users.noreply.github.com>
2023-12-15 09:42:51 +0000

    data types

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer JS version
+
+let name = "Tanim" //string
+let age = 35 //number
+let isLoggedIn = false  //boolean
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true / false
+//null => standalone value
+//undefined =>
+//symbol => unique value
+
+console.log(typeof(age));
+console.log(typeof(null));
+console.log(typeof(undefined));
