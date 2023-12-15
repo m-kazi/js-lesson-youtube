3b5ea19e00944c9f1074803a165c8234fcf1b442
Kazz <38118958+m-kazi@users.noreply.github.com>
2023-12-15 09:19:16 +0000

    variables basic

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const accountName = "Tanim"
+let accountId = "tanim@google.com"
+var accountPassword = "12345"
+accountCity = "New York"
+let accountState;
+
+
+console.log(accountName);
+console.table([accountName, accountId, accountPassword, accountCity, accountState]);
